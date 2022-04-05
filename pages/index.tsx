14bf5a1879577e76a3249a2fa39ac2e9a19b76a6
SiddheshKukade <littlelord124@gmail.com>
2022-04-05 23:57:57 +0530

    Connect Sanity to the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,15 @@ import Image from 'next/image'
 import Posts from '../components/Posts'
 import Header from './../components/Header/'
 import Hero from './../components/Hero/'
-import react from 'react'
-const Home: NextPage = () => {
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from './../typing.d'
+
+interface Props {
+  posts: [Post]
+}
+
+const Home: NextPage = ({ posts }: Props) => {
+  console.log(posts)
   return (
     <div className="mx-auto max-w-7xl">
       <Head>
@@ -19,3 +26,30 @@ const Home: NextPage = () => {
 }
 
 export default Home
+// Making the Home page as a server side rendered page
+// so it's fast and doesn't load the whole app  everytime the user visits the home page
+// and also it is SEO friendly
+export const getServerSideProps = async () => {
+  const query = `
+*[_type=="post"]{
+  title , 
+  _id,
+  
+ author ->{
+ image,
+  name
+},
+description ,
+slug,
+mainImage,
+}
+`
+  const posts = await sanityClient.fetch(query)
+  // sending the props to the component to show it in the UI
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+  
---
+++ sanity.js
@@ -0,0 +1,23 @@
+import { createClient } from 'next-sanity'
+// lib/config.js
+export const config = {     
+    /**
+     * Find your project ID and dataset in `sanity.json` in your studio project.
+     * These are considered “public”, but you can use environment variables
+     * if you want differ between local dev and production.
+     *
+     * https://nextjs.org/docs/basic-features/environment-variables
+     **/
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    apiVersion: '2021-10-21', // Learn more: https://www.sanity.io/docs/api-versioning
+    /**
+     * Set useCdn to `false` if your application require the freshest possible
+     * data always (potentially slightly slower and a bit more expensive).
+     * Authenticated request (like preview) will always bypass the CDN
+     **/
+    useCdn: process.env.NODE_ENV === 'production',
+}
+export const sanityClient = createClient(config);
+//takes source and gives us back the image url
+export const urlFor = (source) => createImageUrlBuilder(config).image(source)
